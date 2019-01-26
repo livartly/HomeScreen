b373a9c6e9258f71afe62d5273cbf973024536b4
Erik Failing <35756586+ewfCode@users.noreply.github.com>
2019-01-26 16:51:01 -0600

    Button Pushing Updates Display

---
+++ Homescreen/Assets/Scripts/ButtonClicks.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClicks : MonoBehaviour {
+
+    //Adds 0 to the passcode
+    public void OnButtonClick_0 ()
+    {
+        GetComponent<Passcode>().passcode += "0";
+    }
+
+    //Adds 1 to the passcode
+    public void OnButtonClick_1()
+    {
+        GetComponent<Passcode>().passcode += "1";
+    }
+
+    //Adds 2 to the passcode
+    public void OnButtonClick_2()
+    {
+        GetComponent<Passcode>().passcode += "2";
+    }
+
+    //Adds 3 to the passcode
+    public void OnButtonClick_3()
+    {
+        GetComponent<Passcode>().passcode += "3";
+    }
+
+    //Adds 4 to the passcode
+    public void OnButtonClick_4()
+    {
+        GetComponent<Passcode>().passcode += "4";
+    }
+
+    //Adds 5 to the passcode
+    public void OnButtonClick_5()
+    {
+        GetComponent<Passcode>().passcode += "5";
+    }
+
+    //Adds 6 to the passcode
+    public void OnButtonClick_6()
+    {
+        GetComponent<Passcode>().passcode += "6";
+    }
+
+    //Adds 7 to the passcode
+    public void OnButtonClick_7()
+    {
+        GetComponent<Passcode>().passcode += "7";
+    }
+
+    //Adds 8 to the passcode
+    public void OnButtonClick_8()
+    {
+        GetComponent<Passcode>().passcode += "8";
+    }
+
+    //Adds 9 to the passcode
+    public void OnButtonClick_9()
+    {
+        GetComponent<Passcode>().passcode += "9";
+    }
+
+}
---
+++ Homescreen/Assets/Scripts/Passcode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Passcode : MonoBehaviour {
+
+    public string passcode;
+    public GameObject display;
+
+    private TextMeshProUGUI go;
+    private const int PASSCODE_LENGTH = 4;
+
+    // Use this for initialization
+    void Start () {
+        passcode = "";
+        go = display.GetComponent<TextMeshProUGUI>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (passcode.Length >= PASSCODE_LENGTH)
+        {
+            //Check passcode
+            CheckPasscode();
+        }
+
+        //Update display to show passcode
+        go.text = passcode;
+	}
+
+    //Checks entered passcode against the randomly generated passcode
+    void CheckPasscode()
+    {
+
+        //Reset the passcode
+        passcode = "";
+    }
+}
