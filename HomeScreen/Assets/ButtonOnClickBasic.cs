521ab991bffde278de6522e94461988cc4b2c2d5
Nick <njcarroll18@gmail.com>
2019-01-25 21:42:43 -0600

    added initial project with initial basic button interface
    Signed-off-by: Nick <njcarroll18@gmail.com>

---
+++ HomeScreen/Assets/ButtonOnClickBasic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOnClickBasic : MonoBehaviour {
+
+
+    public GameObject unlockedImage;
+    public Button unlockButton;
+    // Use this for initialization
+    void Start () {
+        unlockButton.onClick.AddListener(Unlock);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+    void Unlock()
+    {
+        Debug.Log("Phone unlocked");
+        unlockedImage.SetActive(true);
+    }
+}
