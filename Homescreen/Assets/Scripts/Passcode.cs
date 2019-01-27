25a5361061fb856b479fa9f416dd06efa66fccf5
Erik Failing <35756586+ewfCode@users.noreply.github.com>
2019-01-26 21:11:39 -0600

    Minimum Viable Product
    Maximum effort

--- Homescreen/Assets/Scripts/Passcode.cs
+++ Homescreen/Assets/Scripts/Passcode.cs
@@ -8,35 +8,165 @@ public class Passcode : MonoBehaviour {
     public string passcode;
     public GameObject display;
 
-    private TextMeshProUGUI go;
+    private TextMeshProUGUI codeDisplay;
     private const int PASSCODE_LENGTH = 4;
-    private int validPasscode;
+    private string validPasscode;
+    private float timeLeft = 70;
+    public GameObject timeDisplay;
+    private TextMeshProUGUI timer;
+    public GameObject smallTimeDisplay;
+    private TextMeshProUGUI smallTimer;
+    public GameObject content;
+
+    public GameObject Notification;
+
+    private int Attempts = 0;
+
+
+
+    private bool isChecking;
+    private bool isUnlocked;
 
     // Use this for initialization
     void Start () {
         passcode = "";
-        go = display.GetComponent<TextMeshProUGUI>();
+        codeDisplay = display.GetComponent<TextMeshProUGUI>();
+        timer = timeDisplay.GetComponent<TextMeshProUGUI>();
+        smallTimer = smallTimeDisplay.GetComponent<TextMeshProUGUI>();
+
+        isChecking = false;
 
         //Generate a random passcode
-        validPasscode =  (int) Random.Range(0, 9999);
+        validPasscode =  Random.Range(0, 9999).ToString();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (passcode.Length >= PASSCODE_LENGTH)
+        
+
+        if (timeLeft > 0f)
+        {
+            if (isUnlocked == false)
+            {
+                timeLeft -= Time.deltaTime;
+            }
+
+            if (timeLeft < 0)
+            {
+                timeLeft = 0;
+
+            }
+
+
+            //Debug.Log((int)timeLeft);
+            int timeLeftInt = (int)timeLeft;
+            int seconds = timeLeftInt % 60;
+            int minutes = (int)Mathf.Floor(timeLeft / 60);
+            string textString = "";
+            if (minutes < 10)
+                textString += "0";
+            textString += minutes.ToString();
+            textString += ":";
+            if (seconds < 10)
+                textString += "0";
+            textString += seconds.ToString();
+            timer.text = textString;
+            smallTimer.text = textString;
+            //timer.text = timeLeftInt.ToString();
+        }
+
+        if ((passcode.Length >= PASSCODE_LENGTH) && (!isChecking))
         {
             //Check passcode
+            isChecking = true;
             CheckPasscode();
         }
         //Update display to show passcode
-        go.text = passcode;
+        codeDisplay.text = passcode;
 	}
 
     //Checks entered passcode against the randomly generated passcode
     void CheckPasscode()
     {
+        //Tick attempts up
+        Attempts++;
 
-        //Reset the passcode
+        string hint ="";
+        if (passcode == validPasscode)
+        {
+            Unlock();
+        }
+        else
+        {
+            //Check if any numbers match
+            for (int i = 0; i < 4; i += 1)
+            {
+
+                if (validPasscode.Contains(passcode.Substring(i, 1)))
+                {
+
+                    //If valid passcode contains first number
+                    if (passcode[i] == validPasscode[i])
+                    {
+
+
+                        /// right position
+                        hint += "2";
+                    }
+                    else
+                    {
+
+
+                        /// right number wrong position
+
+                        hint += "1";
+                    }
+                }
+                else
+                {
+
+
+                    /// wrong number wrong position
+                    /// 
+                    hint += "0";
+                }
+            }
+            //Instantiate Notification
+            Vector3 offset = new Vector3(50f, -50f * Attempts, 0f);
+            Vector3 nextPos = offset + content.transform.position;
+            GameObject go = Instantiate(Notification, nextPos, Quaternion.identity, content.transform);
+            go.transform.GetComponentInChildren<TextMeshProUGUI>().text = "You entered: " + passcode + "\nHere is your hint: " + hint;
+            Debug.Log(validPasscode);
+        }
+
+        //Wait coroutine 
+        StartCoroutine(Wait());
+    }
+
+    //Wait 
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(.5f);
+        ClearPasscode();
+        isChecking = false;
+    }
+
+    //Reset the passcode
+    void ClearPasscode()
+    {
         passcode = "";
+    }
+
+    void Unlock()
+    {
+        //Stop timer
+        isUnlocked = true;
+
+        GetComponent<ButtonClicks>().OnButtonClick_OpenHomeScreen();
+        Vector3 offset = new Vector3(50f, -50f * Attempts, 0f);
+        Vector3 nextPos = offset + content.transform.position;
+        GameObject go = Instantiate(Notification, nextPos, Quaternion.identity, content.transform);
+        go.transform.GetComponentInChildren<TextMeshProUGUI>().text = "Mom: I'm proud of you <3";
+        Debug.Log("You win");
     }
 }
